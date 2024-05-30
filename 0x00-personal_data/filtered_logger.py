807a6de7cd286c211160499fd7ac07ea429a64b1
aladaabdul <aladarahman18@gmail.com>
2024-05-30 19:40:56 +0100

    filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+"""filter_datum module"""
+
+
+import re
+from typing import List
+
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str
+        ) -> str:
+    pattern = r'({}=).*?(?={})'.format(
+            '|'.join(re.escape(field) for field in fields),
+            re.escape(separator))
+    return re.sub(pattern, r'\1' + redaction, message)
