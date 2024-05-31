a549f6e92b2c01973b028bf45e19e280a8c4c08d
aladaabdul <aladarahman18@gmail.com>
2024-05-31 19:38:24 +0100

    encrypt_password.py

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+"""implement hash_password"""
+
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """hash_password function
+
+    """
+    hash = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    return hash
+
+
+def is_valid(hashed_password: bytes, password: str):
+    """is_valid function
+
+    """
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
