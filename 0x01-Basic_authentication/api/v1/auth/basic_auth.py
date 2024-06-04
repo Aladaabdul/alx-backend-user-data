bf0daf43ee0b02c5854699e1235270476101328b
aladaabdul <aladarahman18@gmail.com>
2024-06-04 19:08:48 +0100

    basic_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,4 +11,18 @@ class BasicAuth(Auth):
     """Basic Auth authentication
 
     """
-    pass
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str
+            ) -> str:
+        """extract_base64_authorization_header function
+
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if authorization_header.startswith('Basic '):
+            return authorization_header.split(' ')[1]
+        else:
+            return None
