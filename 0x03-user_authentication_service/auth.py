0d2f1364b3f366dc638813c1e5be853f6de591e4
aladaabdul <aladarahman18@gmail.com>
2024-06-12 13:15:11 +0100

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,3 +56,16 @@ class Auth:
                         )
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """create_session
+
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                session_id = _generate_uuid()
+                self._db.update_user(user.id, session_id=session_id)
+                return session_id
+        except NoResultFound:
+            return None
